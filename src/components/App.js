386a2301326b9b59bdb0d29120180ce7f03a8b73
Louis Hoang <bawsing@gmail.com>
2018-05-13 13:44:18 -0700

    Add Navbar and basic routes
    To navigate between Home, players and teams page.

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
+import { 
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+import Home from './Home'
+import Player from './Player'
+import Team from './Team'
+import Navbar from './Navbar'
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1 className="App-title">React Router course</h1>
-      </div>
+      <Router>
+        <div>
+          <Navbar/>
+          <Route exact path="/" component={Home}/>
+          <Route path="/players" component={Player}/>
+          <Route path="/teams" component={Team}/>
+        </div>
+      </Router>
     )
   }
 }
---
+++ src/components/Team.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Team extends Component {
+  render(){
+    return (
+      <div className="container two-column">
+       TEAMS
+      </div>
+    )
+  }
+}
+
+export default Team
