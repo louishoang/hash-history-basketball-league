39ba51b8ff7dd5a4462e8451c457f00bc6d2f291
Louis Hoang <bawsing@gmail.com>
2018-05-13 14:10:15 -0700

    Displays teamName and teamLogo on the Homepage
    when click on team logo, user is taken to team's page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react'
+import TeamLogo from './TeamLogo'
+import { Link } from 'react-router-dom'
+import { getTeamNames } from '../api'
 
 class Home extends Component {
+  constructor(){
+    super()
+    this.state = {
+      teamNames: []
+    }
+  }
+
+  componentDidMount(){
+    getTeamNames()
+      .then((teamNames) => this.setState({teamNames}))
+  }
+
   render(){
+    const { teamNames } = this.state
+
     return (
       <div className="container">
-        Home
+        <h1 className="large-header">
+          Hash History Basketball League
+        </h1>
+        <h3 className="header text-center">
+          Select a team
+        </h3>
+        <div className="home-grid">
+          { teamNames.map((id) => (
+            <Link key={id} to={`/${id}`}>
+              <TeamLogo id={id} widht="125px"/>
+            </Link>
+          )) }
+        </div>
       </div>
     )
   }
