6505157d6cabb0988ecfb8252091a9e86f69b471
Louis Hoang <bawsing@gmail.com>
2018-05-13 14:47:17 -0700

    Built a Sidebar on Player page
    Create a sidebar and includes it on the Players page
    
    customLink has 2 properties: to and childen
    + "to" includes a path name and search(query string)
    + childen is whatever element will be printed on the screen between the <li></li> tag
    
    Includes side bar on the Players page
    Note location can only be get from the componentDidMount not inside reander()

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,10 +1,42 @@
 import React, { Component } from 'react'
+// import Sidebar from './Sidebar'
+import { getPlayers } from '../api'
+import Sidebar from './Sidebar';
+import { Route, Link } from 'react-router-dom'
+import { parse } from 'query-string'
+import { slug } from 'slug'
 
 class Player extends Component {
+  constructor() {
+    super()
+    this.state = {
+      players: [],
+      loading: true
+    }
+  }
+
+  componentDidMount() {
+    const { location } = this.props
+    location.search
+      ? this.fetchPlayers(parse(location.search).teamId)
+      : this.fetchPlayers()
+  }
+
+  fetchPlayers = (teamId) => {
+    getPlayers()
+      .then((players) => this.setState({players, loading: false}))
+  }
+
   render(){
+    const { players, loading } = this.state
+
     return (
       <div className="container two-column">
-      Players
+        <Sidebar 
+          title="Players" 
+          list={players.map((player) => player.name)} 
+          loading={loading}
+          {...this.props}/>
       </div>
     )
   }
---
+++ src/components/Sidebar.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Link, Route } from 'react-router-dom'
+import slug from 'slug'
+
+//  Simulate a NavLink from react-router-dom as an example of a custom link
+const CustomLink = ({ to, children }) =>  {
+  return(
+    <Route path={to.pathname}
+      children={({match}) => (
+        <li style={ {listStyleType: 'none', fontWeight: match ? 'bold' : 'normal'}}>
+          <Link to={to}>{children}</Link>
+        </li>
+      )}/>
+  )
+}
+
+const Sidebar = ({ title, list, loading, location, match }) => {
+  return loading === true
+    ? <h1>LOADING</h1>
+    : <div>
+        <h3 className="header">{ title } </h3>
+        <ul className="sidebar-list">
+
+          {
+            list.map((item) => (
+              <CustomLink
+                key={item}
+                children={item}
+                to={{
+                  pathname: `${match.url}/${slug(item)}`,
+                  search: location.search
+                }}/>
+            ))
+          }
+        </ul>
+      </div>
+}
+
+Sidebar.propTypes = {
+  title: PropTypes.string.isRequired,
+  list: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired
+}
+
+export default Sidebar
