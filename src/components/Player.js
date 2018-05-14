3d6599b7db7dc5a586008d35eaf5910f49548884
Louis Hoang <bawsing@gmail.com>
2018-05-13 20:33:29 -0700

    Ability to show each player info on the right side of players page

--- src/components/Player.js
+++ src/components/Player.js
@@ -4,7 +4,7 @@ import { getPlayers } from '../api'
 import Sidebar from './Sidebar';
 import { Route, Link } from 'react-router-dom'
 import { parse } from 'query-string'
-import { slug } from 'slug'
+import slug from 'slug'
 
 class Player extends Component {
   constructor() {
@@ -24,25 +24,56 @@ class Player extends Component {
 
   fetchPlayers = (teamId) => {
     getPlayers(teamId)
-      .then((players) => this.setState({players, loading: false}))
+      .then((players) => this.setState({ players, loading: false }))
   }
 
-  render(){
+  render() {
     const { players, loading } = this.state
-    const { match, location} = this.props
+    const { match, location } = this.props
 
     return (
       <div className="container two-column">
-        <Sidebar 
-          title="Players" 
-          list={players.map((player) => player.name)} 
+        <Sidebar
+          title="Players"
+          list={players.map((player) => player.name)}
           loading={loading}
-          {...this.props}/>
+          {...this.props} />
 
-          {loading === false && location.pathname === '/players'
-            ? <div className="sidebar-instruction">Select a player</div>
-            : null
+        {loading === false && location.pathname === '/players'
+          ? <div className="sidebar-instruction">Select a player</div>
+          : null
         }
+
+        <Route path={`${match.url}/:playerId`} render={({ match }) => {
+          if (loading === true) return null
+
+          const player = players.find((player) => slug(player.name) === match.params.playerId)
+          const {
+            name, position, teamId, number, avatar, apg, ppg, rpg, spg
+          } = player
+
+          return (
+            <div className="panel">
+              <img className="avatar" src={`${avatar}`} alt={`${name}'s avatar`} />
+              <h1 className="medium-header">{name}</h1>
+
+              <h3 className="header">{number}</h3>
+
+              <div className="row">
+                <ul className="info-list" style={{ marginRight: 80 }}>
+                  <li> Team
+                      <div>
+                      <Link style={{ color: "#68809a" }} to={`/${teamId}`}>
+                        {teamId[0].toUpperCase() + teamId.slice(1)}
+                      </Link>
+                    </div>
+                  </li>
+                  <li>Position<div>{position}</div></li>
+                </ul>
+              </div>
+            </div>
+          )
+        }} />
       </div>
     )
   }
